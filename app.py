1e059984ec4546e0f0187131bb3ff976c40c6fae
hidekuma <d.hidekuma@gmail.com>
2019-04-18 23:07:39 +0900

    Updated app.py
    - 불필요한 사항들 삭제처리

--- app.py
+++ app.py
@@ -2,23 +2,14 @@ from flask import Flask, render_template, redirect, request, session, url_for, s
     make_response, jsonify, Response, g, Blueprint
 from flask_restful import reqparse, fields, marshal_with, abort, Api, Resource
 from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_script import Manager
-import configs.flask as fc
-import os
-import re
 import sys
 
 app = Flask(__name__, static_url_path='', static_folder='./static', template_folder='./static')
-app.config.from_object(fc)
 api = Api(app)
 bcrypt = Bcrypt(app)
-db = SQLAlchemy(app)
-manager = Manager(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 
-@manager.command
 def hello():
     print('hello')
 
